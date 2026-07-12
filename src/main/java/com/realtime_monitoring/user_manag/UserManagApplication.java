d0389b4df927f0388ff8539b4c281a318e45a087
ChedlyRebai <chedly.rebai123@gmail.com>
2026-07-12 10:09:13 +0100

    Initialize user management project with Maven setup and Spring Boot application

---
+++ src/main/java/com/realtime_monitoring/user_manag/UserManagApplication.java
@@ -0,0 +1,13 @@
+package com.realtime_monitoring.user_manag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagApplication.class, args);
+	}
+
+}
