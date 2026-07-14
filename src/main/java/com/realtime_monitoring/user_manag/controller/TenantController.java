e7ce944f21bb2fdbdb010818e3e686df12656f89
ChedlyRebai <chedly.rebai123@gmail.com>
2026-07-14 11:23:23 +0100

    Add createTenant to TenantController

--- src/main/java/com/realtime_monitoring/user_manag/controller/TenantController.java
+++ src/main/java/com/realtime_monitoring/user_manag/controller/TenantController.java
@@ -3,15 +3,32 @@ package com.realtime_monitoring.user_manag.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.realtime_monitoring.user_manag.dto.tenant.TenantRequest;
+import com.realtime_monitoring.user_manag.dto.tenant.TenantResponse;
 import com.realtime_monitoring.user_manag.service.TenantService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
-@RequestMapping("/api/tenants")
+@RequestMapping("/api/v1/tenants")
 @RequiredArgsConstructor
 public class TenantController {
     private final TenantService tenantService; 
+
+    @PostMapping
+    public ResponseEntity<TenantResponse> createTenant(@RequestBody TenantRequest entity) {
+        TenantResponse tenantResponse = this.tenantService.createTenant(entity);
+        return ResponseEntity.status(HttpStatus.CREATED).body(tenantResponse);
+    }
+
+    
+    
     
 }
