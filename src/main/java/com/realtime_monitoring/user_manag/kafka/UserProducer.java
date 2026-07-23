16e6cbf7a4ae18d0fe1e4268ca997d08c53d2b3a
ChedlyRebai <chedly.rebai123@gmail.com>
2026-07-23 23:13:32 +0100

    update UserProducer

--- src/main/java/com/realtime_monitoring/user_manag/kafka/UserProducer.java
+++ src/main/java/com/realtime_monitoring/user_manag/kafka/UserProducer.java
@@ -1,13 +1,27 @@
 package com.realtime_monitoring.user_manag.kafka;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import com.realtime_monitoring.user_manag.model.User;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import static org.springframework.kafka.support.KafkaHeaders.TOPIC;
+
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserProducer {
+    private final KafkaTemplate<String, User> kafkaTemplate;
+    public void sendUserCreation(User user ){
+        log.info("sending uuser creation");
+        Message<User> userMessage= MessageBuilder.withPayload(user)
+            .setHeader(TOPIC, "user-creation").build();
+        kafkaTemplate.send(userMessage);
+    }
     
 }
