a7bc40a69bbc32191c799af965deaf8ed1a8ef8d
ChedlyRebai <chedly.rebai123@gmail.com>
2026-07-29 22:29:56 +0100

    create  role producer

--- src/main/java/com/realtime_monitoring/user_manag/kafka/RoleProducer.java
+++ src/main/java/com/realtime_monitoring/user_manag/kafka/RoleProducer.java
@@ -28,6 +28,61 @@ import lombok.extern.slf4j.Slf4j;
 public class RoleProducer {
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    
+    public void sendRoleCreation(Role role) {
+        DomainEvent event = new DomainEvent(
+                UUID.randomUUID(),
+                "ROLE_CREATED",
+                role.getId(),
+                "ROLE",
+                Instant.now());
+
+        RoleCreatedEvent roleEvent = new RoleCreatedEvent(
+                event,
+                role.getId(),
+                role.getName(),
+                role.getDescription()
+                );
+
+        log.info("sending role creation event::::::::::::::: {}", roleEvent);
+        kafkaTemplate.send("role-events-v1", role.getId().toString(), roleEvent);
+    }
+
+    public void sendRoleUpdate(Role role) {
+        DomainEvent event = new DomainEvent(
+                UUID.randomUUID(),
+                "ROLE_UPDATED",
+                role.getId(),
+                "ROLE",
+                Instant.now());
+
+        RoleUpdateEvent roleEvent = new RoleUpdateEvent(
+                event,
+                role.getId(),
+                role.getName(),
+                role.getDescription()
+                );
+        
+
+        log.info("sending role update event::::::::::::::: {}", roleEvent);
+        kafkaTemplate.send("role-events-v1", role.getId().toString(), roleEvent);
+    }
+
+    public void sendTeamCreation(Team team) {
+        DomainEvent event = new DomainEvent(
+                UUID.randomUUID(),
+                "TEAM_CREATED",
+                team.getId(),
+                "TEAM",
+                Instant.now());
+        TeamCreatedEvent teamEvent = new TeamCreatedEvent(
+                team.getId(),
+                event,
+                team.getName(),
+                team.getDescription(),
+                team.getTenantId());
+        log.info("sending team creation event::::::::::::: {}", teamEvent);
+        kafkaTemplate.send("team-events-v2", team.getId().toString(), teamEvent);
+    }
+
 
 }
