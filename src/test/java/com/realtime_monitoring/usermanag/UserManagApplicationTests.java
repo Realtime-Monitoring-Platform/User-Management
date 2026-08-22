f6dab301ea670b16efb713cfa4093975f271eb30
ChedlyRebai <chedly.rebai123@gmail.com>
2026-08-22 09:13:21 +0100

    add appilcation-test

--- src/test/java/com/realtime_monitoring/usermanag/UserManagApplicationTests.java
+++ src/test/java/com/realtime_monitoring/usermanag/UserManagApplicationTests.java
@@ -1,9 +1,14 @@
 package com.realtime_monitoring.usermanag;
 
+import com.realtime_monitoring.usermanag.config.DataInitializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(properties = {"eureka.client.enabled=false", "grpc.server.enabled=false"})
+@ActiveProfiles("test")
+@MockitoBean(types = DataInitializer.class)
 class UserManagApplicationTests {
 
 	@Test
